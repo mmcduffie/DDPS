b50e724e7c93d47f967aaa3698ad22bd0bab8299
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-12-10 10:01:10 -0500

    yea...

--- test_with_nested_array.rb
+++ test_with_nested_array.rb
@@ -24,12 +24,22 @@ class Thunk
 	@content[0][0].push(@used_ids)
     @content[0][1].push(name)
   end
+  def create_call(name,receiver,arguments)
+    @used_ids += 1
+	@content[1][0].push(@used_ids)
+    @content[1][1].push(name)
+  end
 end
 
 class TestThunk < Test::Unit::TestCase
   def test_create_funtion
     test = Thunk.new
 	test.create_function('main')
-	puts test.content.inspect
+	assert_equal "main", test.content[0][1][0]
+  end
+  def test_create_call
+    test = Thunk.new
+	test.create_call('add',5,[5])
+	assert_equal "add", test.content[1][1][0]
   end
 end
